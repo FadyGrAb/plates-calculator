b82c93510aa3b3120372c970afc1c2458c0b4d36
fady-termux <fady.m.louis@gmail.com>
2025-10-04 13:32:05 +0300

    Can build plates inventory now

--- src/PlatesCalculator.java
+++ src/PlatesCalculator.java
@@ -76,56 +76,60 @@ class Handle extends GymEquipment {
 
 }
 	
-class HandleTestDrive {
-	public static void main(String[] args) {
-		Handle barbell = Handle.createNew(6.5, 4);
-		try {
-			System.out.print("Adding 10 Kg plate ... ");
-			barbell.addPlate(new Plate(10));
-			System.out.println("OK");
-			
-			System.out.print("Adding 5 Kg plate ... ");
-			barbell.addPlate(new Plate(5));
-			System.out.println("OK");
-			
-			System.out.print("Adding 1.25 Kg plate ... ");
-			barbell.addPlate(new Plate(1.25));
-			System.out.println("OK");
-			
-			System.out.print("Adding 1.5 Kg plate ... ");
-			barbell.addPlate(new Plate(1.5));
-			System.out.println("OK");
-			
-			assert barbell.getTotalWeigth() == 42.0 : "Total weight should be 42.0";
-
-			System.out.println("Total weight is : " + barbell.getTotalWeigth());
-		} catch (MaxPlatesReachedException e) {
-			// won't happen
-			System.out.println(e.toString());
-		}
-
-		try {
-			System.out.println("Trying to add one more 5 Kg plate...");
-			barbell.addPlate(new Plate(5));
-		} catch (MaxPlatesReachedException e) {
-			System.out.println(e.toString());
-			barbell.detailWeight();
-		}
-	}
-}
-
 public class PlatesCalculator {
+	private Map<Plate, Integer> inventory = new HashMap<>();
+	private Handle handle;
+
 	public static void main(String[] args){
+		PlatesCalculator calc = new PlatesCalculator();
+		calc.buildInventory();
 		
-		System.out.println("Write any thing:");
-		try {
-			double[] inputs = Utils.parsePlatesEntry();
-			System.out.println("Your input: " + Arrays.toString(inputs));
-		} catch (InvalidInputException e) {
-			System.out.println(e.getMessage());
-			System.out.println("Input should be comma seperated pair of PlateWeight(Kg),Count.\nExample: 5,2 // Two 5 Kg plates");
-		}
-
-
 	}
+
+	public void setHandle(double ownWeight, double maxPlatesCount) {
+		handle = new Handle(ownWeight, maxPlatesCount);
+	}
+
+	public Handle getHandle() {
+		return handle;
+	}
+
+	private void buildInventory() {
+		boolean inputComplete = false;
+
+		System.out.println("Build plates inventory:");
+
+		while(!inputComplete){
+			System.out.print("New plate (weight Kg, count): ");
+			try {
+				double[] plateEntry = Utils.parsePlatesEntry();
+				Plate newPlate = new Plate(plateEntry[0]);
+				Integer plateCount = (int) plateEntry[1];
+				addPlateToInventory(newPlate, plateCount); 
+
+				System.out.println("\nCurrent Inventory:");
+				printInventory();
+				System.out.println();
+
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+				System.out.println("Input should be comma seperated pair of PlateWeight(Kg),Count.\nExample: 5,2 // Two 5 Kg plates");
+			}
+		}
+	}
+
+	private void addPlateToInventory(Plate plate, int count) {
+		inventory.put(plate, count);
+	}
+
+	public Map<Plate, Integer> getInventory() {
+		return inventory;
+	}
+
+	public void printInventory() {
+		for (Plate key: inventory.keySet()) {
+			System.out.println(key + " x" + inventory.get(key));
+		}
+	}
+
 }
